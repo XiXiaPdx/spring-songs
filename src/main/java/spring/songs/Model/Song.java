25ef2ac432fc0007c879df4e1eee6868cd7817d6
Xi Xia <xxia@newrelic.com>
2019-11-10 22:00:44 -0800

    Add Id to each song

--- src/main/java/spring/songs/Model/Song.java
+++ src/main/java/spring/songs/Model/Song.java
@@ -2,14 +2,17 @@ package spring.songs.Model;
 
 public class Song {
 
-  private final String artistName;
+  private int id;
+  private String artistName;
   private final String songName;
   private String link;
-  private String lyrics;
+  private final String lyrics;
+  private static int songIdCounter = 0;
 
   @Override
   public String toString() {
     return "Song{" +
+        ", id='" + id + "\"" +
         "artistName='" + artistName + '\'' +
         ", songName='" + songName + '\'' +
         ", link='" + link + '\'' +
@@ -22,10 +25,15 @@ public class Song {
     this.songName = songName;
     this.link = link;
     this.lyrics = lyrics;
+    this.id = ++songIdCounter;
   }
 
   public String getArtistName() {
     return artistName;
+  }
+
+  public void setArtistName(String name){
+    this.artistName = name;
   }
 
   public String getSongName() {
@@ -44,7 +52,7 @@ public class Song {
     return lyrics;
   }
 
-  public void setLyrics(String lyrics) {
-    this.lyrics = lyrics;
+  public int getId() {
+    return id;
   }
 }
